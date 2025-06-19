ae59e793ad1a37f2351ff434ca2812a8e5f15922
Jmar1074 <Jonathanmarin1074@gmail.com>
2025-06-19 11:57:06 -0700

    Update Star_State.py

--- State/Star_State.py
+++ State/Star_State.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
-def initialize_star_state():
-    if "starred" not in st.session_state:
-        st.session_state.starred = []
+def get_starred_tokens():
+    """Retrieve the current list of starred tokens."""
+    return st.session_state.get("starred_tokens", set())
+
+def toggle_star(token_id):
+    """
+    Adds or removes a token from the starred list.
+    Immediately reflects changes in the session state.
+    """
+    if "starred_tokens" not in st.session_state:
+        st.session_state.starred_tokens = set()
+
+    if token_id in st.session_state.starred_tokens:
+        st.session_state.starred_tokens.remove(token_id)
+    else:
+        st.session_state.starred_tokens.add(token_id)

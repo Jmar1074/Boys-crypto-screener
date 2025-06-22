b6b0eb4f7628814c0efb0dce538a0e0d367a6f86
Jmar1074 <Jonathanmarin1074@gmail.com>
2025-06-22 15:21:28 -0700

    Delete UI_Components directory

--- UI_Components/Watchlist.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from ui_components.watchlist_view import render_watchlist
-from state.star_state import get_watchlist
-
-def render_watchlist_page():
-    st.title("⭐ Your Watchlist")
-
-    watchlist = get_watchlist()
-    render_watchlist(watchlist)

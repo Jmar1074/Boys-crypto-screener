41ec3b330c36be5139efc48f08a57ba2fcea81d4
Jmar1074 <Jonathanmarin1074@gmail.com>
2025-06-23 17:53:32 -0700

    Update token_deep_dive.py

--- data_fetch/token_deep_dive.py
+++ data_fetch/token_deep_dive.py
@@ -1,1 +1,27 @@
+from data_fetch.token_details import get_token_details
+from data_fetch.sentiment import get_token_sentiment
+from utils.fallback_request import try_sources
 
+def deep_dive_data(token_id):
+    data_sources = [
+        lambda: get_token_details(token_id),
+        lambda: try_sources(token_id, fallback=True)
+    ]
+
+    for fetcher in data_sources:
+        try:
+            data = fetcher()
+            if data:
+                break
+        except Exception:
+            continue
+    else:
+        return None
+
+    sentiment_score, comments = get_token_sentiment(token_id)
+
+    return {
+        "details": data,
+        "sentiment_score": sentiment_score,
+        "sample_comments": comments
+    }

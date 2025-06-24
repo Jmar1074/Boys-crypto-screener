96dfdf3e41b8e1a54e93332ab073967328de7c05
Jmar1074 <Jonathanmarin1074@gmail.com>
2025-06-23 21:07:04 -0700

    Update token_lookup.py

--- utils/token_lookup.py
+++ utils/token_lookup.py
@@ -1,27 +1,8 @@
 from data_fetch.token_details import get_token_details
-from data_fetch.sentiment import get_token_sentiment
 from utils.fallback_request import try_sources
 
-def token_deep_dive(token_id):
-    detail_sources = [
-        lambda: get_token_details(token_id),
-        lambda: try_sources(token_id, fallback=True)
-    ]
-
-    for fetch in detail_sources:
-        try:
-            data = fetch()
-            if data:
-                break
-        except Exception:
-            continue
-    else:
-        return None
-
-    sentiment_score, comments = get_token_sentiment(token_id)
-
-    return {
-        "details": data,
-        "sentiment_score": sentiment_score,
-        "sample_comments": comments
-    }
+def lookup_token(token_id):
+    data = get_token_details(token_id)
+    if not data:
+        data = try_sources(token_id, fallback=True)
+    return data

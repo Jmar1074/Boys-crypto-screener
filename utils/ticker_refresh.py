3e82e7644fea1df9d96fd01111baf10694aadbc2
Jmar1074 <Jonathanmarin1074@gmail.com>
2025-06-23 17:59:37 -0700

    Update ticker_refresh.py

--- utils/ticker_refresh.py
+++ utils/ticker_refresh.py
@@ -1,1 +1,37 @@
 
+import time
+import threading
+
+class TickerRefresher:
+    def __init__(self, interval=60):
+        self.interval = interval
+        self._data = {}
+        self._lock = threading.Lock()
+        self._running = False
+        self._thread = None
+
+    def start(self, fetch_function):
+        if not self._running:
+            self._running = True
+            self._thread = threading.Thread(target=self._refresh_loop, args=(fetch_function,))
+            self._thread.daemon = True
+            self._thread.start()
+
+    def _refresh_loop(self, fetch_function):
+        while self._running:
+            try:
+                data = fetch_function()
+                with self._lock:
+                    self._data = data
+            except Exception:
+                pass
+            time.sleep(self.interval)
+
+    def get_data(self):
+        with self._lock:
+            return self._data.copy()
+
+    def stop(self):
+        self._running = False
+        if self._thread:
+            self._thread.join()

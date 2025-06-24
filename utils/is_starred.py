3c06c11b3cd393d400652961376a1c3d8c9e1b66
Jmar1074 <Jonathanmarin1074@gmail.com>
2025-06-23 21:53:31 -0700

    Update is_starred.py

--- utils/is_starred.py
+++ utils/is_starred.py
@@ -1,12 +1,2 @@
-import json
-import os
-
-WATCHLIST_FILE = os.path.join("data", "watchlist.json")
-
-def is_starred(token_id):
-    try:
-        with open(WATCHLIST_FILE, "r") as f:
-            watchlist = json.load(f)
-        return token_id in watchlist
-    except Exception:
-        return False
+def is_starred(symbol, starred_list):
+    return symbol in starred_list

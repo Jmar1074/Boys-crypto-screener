dc08f74b7df04fcc3774d64a4e833071cb22bd2c
Jmar1074 <Jonathanmarin1074@gmail.com>
2025-06-23 17:58:42 -0700

    Update fallback_request.py

--- utils/fallback_request.py
+++ utils/fallback_request.py
@@ -1,1 +1,19 @@
+import requests
 
+def try_sources(token_id, fallback=False):
+    sources = [
+        f"https://api.coingecko.com/api/v3/coins/{token_id}",
+        f"https://api.coinpaprika.com/v1/coins/{token_id}"
+    ]
+
+    for source in sources:
+        try:
+            response = requests.get(source)
+            if response.status_code == 200:
+                return response.json()
+        except Exception:
+            continue
+
+    if fallback:
+        return {"message": "All fallback sources failed."}
+    return None
